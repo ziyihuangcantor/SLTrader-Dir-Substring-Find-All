175d5225bfa58f30c254780153c3b1b282deff31
ziyihuangcantor <107073582+ziyihuangcantor@users.noreply.github.com>
2022-06-30 16:58:09 -0400

    Added screen column and export to excel

--- Areas/Locates/Models/InventoryLocateModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SLTrader.Models
-{
-    public class InventoryLocateModel
-    {
-        public int      IssueId { get; set; }
-        public string   Source { get; set; }
-        public decimal  Available { get; set; }
-    }
-}
